f9cbd0ed6a1adde6e7fae83f770a1870bb6738a3
devraj-alt <mr.devrajroy@outlook.com>
2021-09-28 13:41:54 +0530

    style: responsiveness improved

--- calculateHypotenuse.js
+++ calculateHypotenuse.js
@@ -1,20 +1,21 @@
-const sides = document.querySelectorAll('.input-field');
-const submitButton = document.querySelector('#submit-btn');
-const displayAnswer = document.querySelector('#display-answer');
-
+const sides = document.querySelectorAll(".input-field");
+const submitButton = document.querySelector("#submit-btn");
+const displayAnswer = document.querySelector("#display-answer");
 
 function calculateSumOfSquares(a, b) {
-    const sumOfSquares = a * a + b * b;
-    return sumOfSquares;
+  const sumOfSquares = a * a + b * b;
+  return sumOfSquares;
 }
-
 
 function calculateHypotenuse() {
-    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value), Number(sides[1].value))
-    const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
+  const sumOfSquares = calculateSumOfSquares(
+    Number(sides[0].value),
+    Number(sides[1].value)
+  );
+  const lengthOfHypotenuse = Math.sqrt(sumOfSquares);
 
-    displayAnswer.innerText = "Hypotenuse is " + lengthOfHypotenuse + " cm";
+  displayAnswer.innerText =
+    "Hypotenuse is " + lengthOfHypotenuse.toFixed(2) + " cm";
 }
 
-
-submitButton.addEventListener('click', calculateHypotenuse);
+submitButton.addEventListener("click", calculateHypotenuse);
